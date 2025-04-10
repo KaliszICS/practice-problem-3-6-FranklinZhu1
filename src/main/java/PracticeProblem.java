3d93f95f93cf3ef56ad0cc98c64b15b572cc6245
franklin-zhu-ics4u1-02-25 <123124410+FranklinZhu1@users.noreply.github.com>
2025-04-10 13:32:56 +0000

    Create solution methods

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,25 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static int dfs(String[][] arr, int r, int c, int moves) {
+		if (r < 0 || c > arr[r].length - 1 || arr[r][c].equals("*")) return 999999; // wall or out of bounds
+		if (arr[r][c].equals("F")) return moves; // reached finish
+		return Math.min(dfs(arr, r - 1, c, moves + 1), dfs(arr, r, c + 1, moves + 1));
 	}
 
+	public static int searchMazeMoves(String[][] arr) {
+		int ans = dfs(arr, arr.length - 1, 0, 0);
+		if (ans == 999999) ans = -1;
+		return ans;
+	}
+
+	public static int dfsPathCount(String[][] arr, int r, int c) {
+		if (r < 0 || c > arr[r].length - 1 || arr[r][c].equals("*")) return 0;
+		if (arr[r][c].equals("F")) return 1;
+		return dfsPathCount(arr, r - 1, c) + dfsPathCount(arr, r, c + 1);
+	}
+
+	public static int noOfPaths(String[][] arr) {
+		return dfsPathCount(arr, arr.length - 1, 0);
+	}
 	
 }
